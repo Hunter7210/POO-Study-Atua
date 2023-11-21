5550548a1fcf92e5a9fb69e6d36f9a5197b0aa18
Hunter7210 <heitorrmsilva@gmail.com>
2023-11-21 16:56:17 -0300

    21/11/2023

--- guijdbclojacarros/src/main/java/View/VendasView.java
+++ guijdbclojacarros/src/main/java/View/VendasView.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import Connection.CarrosDAO;
 import Connection.ClientesDAO;
@@ -16,9 +19,14 @@ public class VendasView extends JPanel {
 
     private JComboBox<String> carrosComboBox;
     private List<Carros> carros;
-    private JButton enviar, limpar;
+    private JButton enviar, limpar, historico;
     private JComboBox<String> clienteComboBox;
     private List<Clientes> clientes;
+
+    private JTable table;
+    private DefaultTableModel tableModel;
+    private int linhaSelecionada = -1; // Valor para quando não selecionar nada
+
 
     public VendasView() {
         super();
@@ -51,6 +59,26 @@ public class VendasView extends JPanel {
         //Adiciona os componentes
         painelPrinc.add(carrosComboBox);
         painelPrinc.add(clienteComboBox);
+
+        //Criação de um painel para conter os botoes
+        JPanel botoes = new JPanel();
+        painelPrinc.add(botoes);
+        enviar = new JButton("Comprar");
+        historico = new JButton("Histórico");
+        limpar = new JButton("Limpar");
+
+        botoes.add(enviar);
+        botoes.add(limpar);
+        botoes.add(historico);
+
+         // tabela de carros
+        JScrollPane jSPane = new JScrollPane();
+        add(jSPane);
+
+        tableModel = new DefaultTableModel(new Object[][] {},
+                new String[] { "Marca", "Modelo", "Ano", "Placa", "Valor" });
+        table = new JTable(tableModel);
+        jSPane.setViewportView(table);
     }
 
 }
